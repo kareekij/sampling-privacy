4c696f6391b9294803b916c9e5d4c0169f04b1b7
kpugdeet <krittaphat.pug@gmail.com>
2017-04-17 08:11:52 -0400

    Update Threshold to stop running NN

--- attack/NnTf.py
+++ attack/NnTf.py
@@ -5,14 +5,29 @@ import time
 
 class NN:
     def __init__(self, xDim, yDim):
-        with tf.device('/gpu:0'):
+        with tf.device('/cpu:0'):
+            self.xDim = xDim
+            self.yDim = yDim
             size = (xDim+yDim)/2
+            # size = xDim
             # Initial all variable and equation graph
             self.x = tf.placeholder(tf.float32, [None, xDim])
             self.y = tf.placeholder(tf.float32, [None, yDim])
 
             self.wH1 = tf.Variable(tf.random_normal([xDim, size]), trainable=True)
             self.y1 = tf.sigmoid(tf.matmul(self.x, self.wH1))
+
+            # self.wH2 = tf.Variable(tf.random_normal([size, size]), trainable=True)
+            # self.y2 = tf.sigmoid(tf.matmul(self.y1, self.wH2))
+            #
+            # self.wH3 = tf.Variable(tf.random_normal([size, size]), trainable=True)
+            # self.y3 = tf.sigmoid(tf.matmul(self.y2, self.wH3))
+            #
+            # self.wH4 = tf.Variable(tf.random_normal([size, size]), trainable=True)
+            # self.y4 = tf.sigmoid(tf.matmul(self.y3, self.wH4))
+            #
+            # self.wH5 = tf.Variable(tf.random_normal([size, size]), trainable=True)
+            # self.y5 = tf.sigmoid(tf.matmul(self.y4, self.wH5))
 
             self.W = tf.Variable(tf.random_normal([size, yDim]), trainable=True)
             self.y_ = tf.sigmoid(tf.matmul(self.y1, self.W))
@@ -28,15 +43,24 @@ class NN:
             self.sess = tf.Session()
             self.sess.run(self.init)
 
+            # For Tensorboard
+            # tf.summary.scalar("Cost", self.costFunction)
+            # tf.summary.scalar("Accuracy", self.accuracy)
+            # self.merge = tf.summary.merge_all()
+            # self.write = tf.summary.FileWriter("./logs", self.sess.graph)
+
     def fit(self, x, y, epoch):
         # Train
         for _ in range(epoch):
-            self.sess.run(self.optimizer, feed_dict={self.x: x, self.y: y})
-            self.sess.run(self.costFunction, feed_dict={self.x: x, self.y: y})
+            tmp, cost = self.sess.run([self.optimizer,self.costFunction], feed_dict={self.x: x, self.y: y})
+            # summary, tmp, cost = self.sess.run([self.merge, self.optimizer, self.costFunction], feed_dict={self.x: x, self.y: y})
+            # self.write.add_summary(summary, _)
+            if cost < self.xDim*self.yDim*0.01:
+                print _
+                break
 
         # Test the model
         print("Training Accuracy: {0}".format(self.sess.run(self.accuracy, feed_dict={self.x: x, self.y: y})))
-        tf.summary.FileWriter("./logs", self.sess.graph)
 
     def predict(self, x):
         return self.sess.run(tf.argmax(self.y_, 1), feed_dict={self.x: x})
