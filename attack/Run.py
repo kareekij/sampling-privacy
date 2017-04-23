f3bb53410f4c033bb4b3cd54c53a4282355347dc
kpugdeet <krittaphat.pug@gmail.com>
2017-04-23 13:23:15 -0400

    Add more classifier

--- attack/Run.py
+++ attack/Run.py
@@ -2,7 +2,10 @@ import numpy as np
 from sklearn import svm
 from sklearn.neural_network import MLPClassifier
 from imblearn.under_sampling import RandomUnderSampler
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import roc_auc_score
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 import sys
 import glob
 import os
@@ -14,7 +17,9 @@ from extract_features import ExtractFeatures
 f = open("./" + sys.argv[5] +"/output_"+sys.argv[1]+"_"+sys.argv[2].replace("/", "_")+".csv","w")
 
 for loop in range(int(sys.argv[4])):
-    # f.write("FILENAME, RAND, RAND.Pred, RAND.Act, RAND.All, BASIC, BASIC.Pred, BASIC.Act, BASIC.All, AGG, AGG.Pred, AGG.Act, AGG.All, BLOCK, BLOCK.Pred, BLOCK.Act, BLOCK.All, SVM, SVM.Pred, SVM.Act, SVM.All, SVM.Auc, NN, NN.Pred, NN.Act, NN.All, Folder, Category, Trial\n")
+    # f.write("FILENAME, RAND, RAND.Pred, RAND.Act, RAND.All, BASIC, BASIC.Pred, BASIC.Act, BASIC.All, AGG, AGG.Pred, AGG.Act, AGG.All, BLOCK, BLOCK.Pred, BLOCK.Act, BLOCK.All, SVMR, SVMR.Pred, SVMR.Act, SVMR.All, NN, NN.Pred, NN.Act, NN.All,
+    # NB, NB.Pred, NB.Act, NB.All, DT, DT.Pred, DT.Act, DT.All, RDF, RDF.Pred, RDF.Act, RDF.All, ABD, ABD.Pred, ABD.Act, ABD.All, SVML, SVML.Pred, SVML.Act, SVML.All,
+    # SVMP, SVMP.Pred, SVMP.Act, SVMP.All, SVMS, SVMS.Pred, SVMS.Act, SVMS.All, Folder, Category, Trial\n")
 
     for fileName in glob.glob("../sampling/" + sys.argv[5] +"/"+sys.argv[2]+"/*"):
         print "Processing:      {0}".format(os.path.basename(fileName))
@@ -148,18 +153,19 @@ for loop in range(int(sys.argv[4])):
         xReSampled, yReSampled, idxReSampled = rus.fit_sample(trainX, trainY)
 
 
-        # Attack with SVM
-        print "SVM:   ",
-        clf = svm.SVC()
+        # Attack with SVM RBF
+        print "SVMR:  ",
+        clf = svm.SVC(kernel='rbf')
         yScore = clf.fit(xReSampled, yReSampled).decision_function(testX)
         predict = clf.predict(testX)
         accuracy = list(predict == np.array(testY))
         print "{0:13}".format((int(predict[newTarget] == testY[newTarget]), predict[newTarget], testY[newTarget])),
         f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(testY[newTarget]))
-        print " {0:13}".format((accuracy.count(True) / float(len(accuracy)))),
+        print " {0:13}".format((accuracy.count(True) / float(len(accuracy))))
         f.write("," + str (accuracy.count(True) / float(len(accuracy))))
-        print " {0}".format(roc_auc_score(testY, yScore))
-        f.write("," + str(roc_auc_score(testY, yScore)))
+        # print " {0}".format(roc_auc_score(testY, yScore))
+        # f.write("," + str(roc_auc_score(testY, yScore)))
+
 
         # Attack with NN
         print "NN:    ",
@@ -171,6 +177,104 @@ for loop in range(int(sys.argv[4])):
         f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(testY[newTarget]))
         print " {0}".format((accuracy.count(True) / float(len(accuracy))))
         f.write("," + str(accuracy.count(True) / float(len(accuracy))))
+
+
+        # Attack with Naive Bay
+        print "NB:    ",
+        clf = GaussianNB()
+        clf.fit(xReSampled, yReSampled)
+        predict = clf.predict(testX)
+        accuracy = list(predict == np.array(testY))
+        print "{0:13}".format((int(predict[newTarget] == testY[newTarget]), predict[newTarget], testY[newTarget])),
+        f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(
+            testY[newTarget]))
+        print " {0}".format((accuracy.count(True) / float(len(accuracy))))
+        f.write("," + str(accuracy.count(True) / float(len(accuracy))))
+
+
+        # Attack with Decision Tree
+        print "DT:    ",
+        clf = tree.DecisionTreeClassifier()
+        clf.fit(xReSampled, yReSampled)
+        predict = clf.predict(testX)
+        accuracy = list(predict == np.array(testY))
+        print "{0:13}".format((int(predict[newTarget] == testY[newTarget]), predict[newTarget], testY[newTarget])),
+        f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(
+            testY[newTarget]))
+        print " {0}".format((accuracy.count(True) / float(len(accuracy))))
+        f.write("," + str(accuracy.count(True) / float(len(accuracy))))
+
+
+        # Attack with Random Forest
+        print "RDF:   ",
+        clf = RandomForestClassifier(n_estimators=10)
+        clf.fit(xReSampled, yReSampled)
+        predict = clf.predict(testX)
+        accuracy = list(predict == np.array(testY))
+        print "{0:13}".format((int(predict[newTarget] == testY[newTarget]), predict[newTarget], testY[newTarget])),
+        f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(
+            testY[newTarget]))
+        print " {0}".format((accuracy.count(True) / float(len(accuracy))))
+        f.write("," + str(accuracy.count(True) / float(len(accuracy))))
+
+
+        # Attack with AdaBoost
+        print "ADB:   ",
+        clf = AdaBoostClassifier(n_estimators=100)
+        clf.fit(xReSampled, yReSampled)
+        predict = clf.predict(testX)
+        accuracy = list(predict == np.array(testY))
+        print "{0:13}".format((int(predict[newTarget] == testY[newTarget]), predict[newTarget], testY[newTarget])),
+        f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(
+            testY[newTarget]))
+        print " {0}".format((accuracy.count(True) / float(len(accuracy))))
+        f.write("," + str(accuracy.count(True) / float(len(accuracy))))
+
+
+        # Attack with SVM Linear
+        print "SVML:  ",
+        clf = svm.SVC(kernel='linear')
+        yScore = clf.fit(xReSampled, yReSampled).decision_function(testX)
+        predict = clf.predict(testX)
+        accuracy = list(predict == np.array(testY))
+        print "{0:13}".format((int(predict[newTarget] == testY[newTarget]), predict[newTarget], testY[newTarget])),
+        f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(
+            testY[newTarget]))
+        print " {0:13}".format((accuracy.count(True) / float(len(accuracy))))
+        f.write("," + str(accuracy.count(True) / float(len(accuracy))))
+        # print " {0}".format(roc_auc_score(testY, yScore))
+        # f.write("," + str(roc_auc_score(testY, yScore)))
+
+
+        # Attack with SVM Poly
+        print "SVMP:  ",
+        clf = svm.SVC(kernel='poly')
+        yScore = clf.fit(xReSampled, yReSampled).decision_function(testX)
+        predict = clf.predict(testX)
+        accuracy = list(predict == np.array(testY))
+        print "{0:13}".format((int(predict[newTarget] == testY[newTarget]), predict[newTarget], testY[newTarget])),
+        f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(
+            testY[newTarget]))
+        print " {0:13}".format((accuracy.count(True) / float(len(accuracy))))
+        f.write("," + str(accuracy.count(True) / float(len(accuracy))))
+        # print " {0}".format(roc_auc_score(testY, yScore))
+        # f.write("," + str(roc_auc_score(testY, yScore)))
+
+
+        # Attack with SVM Sigmoid
+        print "SVMS:  ",
+        clf = svm.SVC(kernel='sigmoid')
+        yScore = clf.fit(xReSampled, yReSampled).decision_function(testX)
+        predict = clf.predict(testX)
+        accuracy = list(predict == np.array(testY))
+        print "{0:13}".format((int(predict[newTarget] == testY[newTarget]), predict[newTarget], testY[newTarget])),
+        f.write("," + str(int(predict[newTarget] == testY[newTarget])) + "," + str(predict[newTarget]) + "," + str(
+            testY[newTarget]))
+        print " {0:13}".format((accuracy.count(True) / float(len(accuracy))))
+        f.write("," + str(accuracy.count(True) / float(len(accuracy))))
+        # print " {0}".format(roc_auc_score(testY, yScore))
+        # f.write("," + str(roc_auc_score(testY, yScore)))
+
 
         # New line
         f.write("," + sys.argv[6] + "," + sys.argv[3] + "," + str(loop))
